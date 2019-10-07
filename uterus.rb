a458298d3ad7dd86dfc79f2d916723dd8f33e537
sqdorte <sqd0rte@Protonmail.com>
2019-10-06 23:26:07 -0300

    listen and store messages

--- uterus.rb
+++ uterus.rb
@@ -3,14 +3,34 @@ require 'json'
 
 secrets = JSON.parse(File.read('secrets.json'))
 token = secrets['token']
+channel = secrets['channel_id']
 
 Telegram::Bot::Client.run(token) do |bot|
-  # botUser = bot.getMe()
-  # print "logged in as {botUser.first_name} {botUser.last_name} (@{botUser.username}) [{botUser.id}]"
+  botUser = bot.api.get_me()
+  if botUser["ok"]
+    result = botUser["result"]
+    puts "success"
+    puts "logged in as #{result['first_name']} (@#{result['username']}) [#{result['id']}]"
+  else
+    puts "failed"
+    exit
+  end
 
   bot.listen do |message|
-    if message.chat.id == -1001451190298
-      # reee
+    sender = message.from
+    
+    if message.chat.id == channel && !sender.is_bot && !message.text.start_with?("/")
+      data = {}
+      data_path = "/data/#{sender.id}.json"
+
+      if File.file?(data_path)
+        data = JSON.parse(File.read('data_path'))
+      end
+
+      words = [nil] + message.text.split + [nil]
+      words[0..-2].zip(words[1..-1]) { |word,next_word|
+        
+      }
     end
   end
 end
